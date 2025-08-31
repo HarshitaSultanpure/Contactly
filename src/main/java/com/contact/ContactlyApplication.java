5cf5bb9fe99e425fae6f0f8dfaf6c041fc0833c7
HarshitaSultanpure <harshitasultan30304@gmail.com>
2025-09-01 00:01:49 +0530

    init

---
+++ src/main/java/com/contact/ContactlyApplication.java
@@ -0,0 +1,13 @@
+package com.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactlyApplication.class, args);
+	}
+
+}
