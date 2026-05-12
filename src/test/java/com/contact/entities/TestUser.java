0ac3aafe3119077e117804aba9430d1afd957458
HarshitaSultanpure <harshitasultan30304@gmail/com>
2026-05-12 17:51:47 +0530

    Added unit tests for User entity

--- src/test/java/com/contact/entities/TestUser.java
+++ src/test/java/com/contact/entities/TestUser.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UserTest {
+public class TestUser {
 
     private User user;
 
